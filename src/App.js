c9a08514f7be860f8f226a00080d9be5de6beec1
IgorKudlo <igoreklida@gmail.com>
2020-11-24 21:54:48 +0300

    add router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import {Home} from './pages/Home'
+import {About} from './pages/About'
+import {Navbar} from './components/Navbar'
 
 function App() {
   return (
-    <div className="container">
-      <h1>Note App</h1>
-    </div>
+    <Router>
+      <Navbar />
+      <div className="container pt-4">
+        <Switch>
+          <Route path={'/'} exact component={Home} />
+          <Route path={'/about'} component={About} />
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
