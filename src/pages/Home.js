0f475cedce35aec368a228c73419a63b9765ef06
Igor Kudlo <igoreklida@gmail.com>
2020-11-25 12:32:31 +0300

    create components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,18 @@
 import React, {Fragment} from 'react'
+import {Form} from '../components/Form'
+import {Notes} from '../components/Notes'
 
 export const Home = () => {
+
+  const notes = new Array(3)
+    .fill('')
+    .map((_, i) => ({id: 1, title: `Note ${i + 1}`}))
+
   return (
     <Fragment>
-      <h1>Home page</h1>
+      <Form />
+      <hr />
+      <Notes notes={notes} />
     </Fragment>
   )
 }
