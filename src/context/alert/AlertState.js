f19258a7f060c1d89a024fea0dd58901905de38e
Igor Kudlo <igoreklida@gmail.com>
2020-11-25 15:53:21 +0300

    alert context

---
+++ src/context/alert/AlertState.js
@@ -0,0 +1,13 @@
+import React, {useReducer} from 'react'
+import {AlertContext} from './alertContext'
+import {alertReducer} from './alertReducer'
+
+export const AlertState = ({children}) => {
+  const [state, dispatch] = useReducer(alertReducer, {visible: false})
+
+  return (
+    <AlertContext.Provider>
+      {children}
+    </AlertContext.Provider>
+  )
+}
